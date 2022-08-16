a6cf4132cbd8deaf0b8138f1926357a9104aaf2a
Snowflakeeee <snigana.pylypyuk@gmail.com>
2022-08-16 23:02:32 +0200

    second_task

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,13 @@
-# from pages.base_page import BasePage
-#
-#
-# class LoginPage(BasePage):
-#     login_field_xpath = "//*[@id='login']"
-#     password_field_xpath =
-#     sign_in_button_xpath =
-#
-#     def type_in_email(self, email):
-#         self.field_send_keys(self.login_field_xpath, email)
+from pages.base_page import BasePage
+
+
+class LoginPage(BasePage):
+    login_field_xpath = "//*[@id='login']"
+    password_field_xpath ="//body[1]/div[1]/form[1]/div[1]/div[1]/div[2]/div[1]/input[1]"
+    sign_in_button_xpath ="//span[contains(text(),'Sign in')]"
+    scouts_panel_xpath ="//h5[contains(text(),'Scouts Panel')]"
+    remind_password_xpath ="//a[contains(text(),'Remind password')]"
+    change_language_xpath ="//body[1]/div[1]/form[1]/div[1]/div[2]/div[1]/div[1]"
+
+    def type_in_email(self, email):
+        self.field_send_keys(self.login_field_xpath, email)
